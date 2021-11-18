b46b4fbf74dc9128fa967de3727853d50a20029d
Praveen Krishnamoorthy <pkrishnamoorthy@excentus.com>
2021-11-18 17:20:17 +0530

    First Commit from User 1

---
+++ src/test/java/User1_Package1/user1_Class1.java
@@ -0,0 +1,11 @@
+package User1_Package1;
+
+public class user1_Class1 {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("User_1 Package_1 Class_1");
+		
+	}
+
+}
---
+++ src/test/java/User1_Package2/user1_Class2.java
@@ -0,0 +1,11 @@
+package User1_Package2;
+
+public class user1_Class2 {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("User_1 Package_2 Class_2");
+		
+	}
+
+}
