2266700d0638c0346d764540ea2d71a1e798b290
Praveen Krishnamoorthy <pkrishnamoorthy@excentus.com>
2021-11-18 17:42:20 +0530

    User2 Branch - First Commit

---
+++ src/test/java/User2_Branch1_Package1/user2_Branch1_Class1.java
@@ -0,0 +1,11 @@
+package User2_Branch1_Package1;
+
+public class user2_Branch1_Class1 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("User_2 Branch_1 Package_1 Class_1");
+		
+	}
+	
+}
